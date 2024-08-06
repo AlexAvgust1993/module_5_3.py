b24e1e1d6c5dd8063f4cb84000268160f5f2a9f6
Alex <alexavgust1993@gmail.com>
2024-08-06 17:21:12 +0300

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,67 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name  # Название
+        self.number_of_floors = number_of_floors  # Количество этажей (18 и 2 этажа)
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):  # Сравнили текущие с представленными этажам № 1
+        return self.number_of_floors == other.number_of_floors
+
+    def __add__(self, other):  # Прибавили к текущим этажам № 2
+        self.number_of_floors += other
+        return self
+
+    def __iadd__(self, other):  # Арифметическое присваивание
+        return self + other
+
+    def __radd__(self, value):  # Обратное сложение
+        return self.__add__(value)
+
+# А дальше пойдут сравнения
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print("Такого этажа не существует")
+        else:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2)  # __eq__
+h1 = h1 + 10  # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10  # __iadd__
+print(h1)
+
+h2 = 10 + h2  # __radd__
+print(h2)
+
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
